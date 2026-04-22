1b34286f7697c93c7f6d5309a72946844819c8f2
João Furlan <joao.fuurlan@gmail.com>
2026-04-22 08:16:18 -0300

    feat: create log file for API

--- src/middleware/logger.py
+++ src/middleware/logger.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import os
 
 # Definição de cores ANSI
 RESET = "\033[0m"
@@ -19,15 +20,35 @@ def get_logger(name: str):
         # Propagate=False evita que o log suba para o root e duplique a mensagem
         logger.propagate = False
 
-        # Formato: Data - Nome do Arquivo - Nível - Mensagem
-        formatter = logging.Formatter(
-            "%(asctime)s - [%(name)s] - %(levelname)-8s - %(message)s",
-            datefmt="%H:%M:%S"
+        # 1. FORMATADORES
+        # Formato com cores para o terminal (melhor para leitura)
+        shell_formatter = logging.Formatter(
+            f"{CYAN}%(asctime)s{RESET} - [%(name)s] - {BOLD}%(levelname)-8s{RESET} - %(message)s", datefmt="%H:%M:%S"
         )
 
-        # Log no terminal
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        # Formato limpo para o arquivo (sem códigos de cor ANSI)
+        file_formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+
+        # 2. HANDLER TERMINAL
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(shell_formatter)
+        logger.addHandler(stream_handler)
+
+
+        # 3. HANDLER ARQUIVO
+        log_dir = os.path.join("reports", "logs")
+        os.makedirs(log_dir, exist_ok=True)
+
+
+        file_handler = logging.FileHandler(
+                                    os.path.join(log_dir, "api.log"),
+                                    mode="a", 
+                                    encoding="utf-8"
+                                    )
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
 
     return logger
